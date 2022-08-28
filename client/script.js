626db4ad7d63d3f1376b585378a8fbf3f030ef6e
linnamuri <105757219+linnamuri@users.noreply.github.com>
2022-08-28 10:43:01 -0400

    adding comments

--- client/script.js
+++ client/script.js
@@ -1,17 +1,28 @@
+//This element in HTML will be holding recipe cards
 const recipesContainer = document.getElementById('recipesContainer')
-const form = document.querySelector('form')
+//const form = document.querySelector('form')
 
+//End Points for Get, Create and Delete Recipes
 const getrecipeURL =`http://localhost:4000/api/getallrecipes`
 const postrecipeURL =`http://localhost:4000/api/createrecipe`
 const deleterecipeURL =`http://localhost:4000/api/deleterecipe`
 
-
+//Callback method after receiving response from server
 const recipesCallback = ({ data: recipes }) => displayRecipes(recipes)
+
+//Axios GET call to get all recipes
 const getAllRecipes = () => axios.get(getrecipeURL).then(recipesCallback).catch(errCallback)
+
+//Error Callback Method. This is called if the server is returning an error.
 const errCallback = err => console.log(err.response.data)
+
+//Axios POST call to create recipe
 const createRecipe = body => axios.post(postrecipeURL, body).then(recipesCallback).catch(errCallback)
+
+//Axios DELETE call to delete recipe
 const deleteRecipe = id => axios.delete(`${deleterecipeURL}/${id}`).then(recipesCallback).catch(errCallback)
 
+//This method is called when the Submit button is clicked for Create Recipe form. This function constructs the POST object and calls createRecipe method.
 function submitHandler(e) {
     e.preventDefault()
 
@@ -38,6 +49,8 @@ function submitHandler(e) {
     document.getElementById("message").innerHTML="<br/><br/>Recipe added succesfully"
 
 }
+
+//This function is to create recipe cards and iterates through response JSON array received from server
 function createRecipeCard(recipe) {
     const recipeCard = document.createElement('div')
     recipeCard.classList.add('flip-card')
@@ -59,6 +72,7 @@ function createRecipeCard(recipe) {
     recipesContainer.appendChild(recipeCard)
 }
 
+//This is callback function that is called after receiving data from server
 function displayRecipes(arr) {
     recipesContainer.innerHTML = ''
     for (let i = 0; i < arr.length; i++) {
@@ -66,6 +80,7 @@ function displayRecipes(arr) {
     }
 }
 
+//This method is to show Create Recipe form
 function showRecipeForm()
 {
     document.getElementById("recipe-form").style.display="block"
